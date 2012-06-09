bbf5ba8e7377e5c087323d2b2947ce0e4635938a
igel <igel.yulian@mail.ru>
2012-06-09 12:42:50 +0400

    cap3

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,7 +66,7 @@ set :scm,             :git
 # домашнем каталоге в подкаталоге git/<имя проекта>.git.
 # Подробнее о создании репозитория читайте в нашем блоге
 # http://locum.ru/blog/hosting/git-on-locum
-set :repository,      "git://github.com/igel84/tekstil.git"
+set :repository,      "git@github.com:igel84/tekstil.git"
 
 ## Если ваш репозиторий в GitHub, используйте такую конфигурацию
 # set :repository,    "git@github.com:username/project.git"
